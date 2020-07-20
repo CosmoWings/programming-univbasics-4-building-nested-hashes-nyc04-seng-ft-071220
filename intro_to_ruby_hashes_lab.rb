6cc2688b648778582996c2e7bb56eac22d9fade5
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-20 14:17:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   lab_hash = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
+    :railroads=> {
+      :pieces=> 4,
+      :rent_in_dollars=> {},
+      :names=> {}
     }
   }
 
@@ -28,19 +28,19 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   lab_hash = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
+    :railroads=> {
+      :pieces=> 4,
+      :rent_in_dollars=> {
+        :one_piece_owned=> 25,
+        :two_pieces_owned=> 50,
+        :three_pieces_owned=> 100,
+        :four_pieces_owned=> 200
       },
-      :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}
+      :names=> {
+        :reading_railroad=> {},
+        :pennsylvania_railroad=> {},
+        :b_and_o_railroad=> {},
+        :shortline_railroad=> {}
       }
     }
   }
@@ -54,17 +54,17 @@ def monopoly_with_fourth_tier
   # complexity.
   :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
+      :rent_in_dollars=> {
+        :one_piece_owned=> 25,
+        :two_pieces_owned=> 50,
+        :three_pieces_owned=> 100,
+        :four_pieces_owned=> 200
       },
-      :names => {
-        :reading_railroad =>{:mortgage_value => 100},
-        :pennsylvania_railroad =>{:mortgage_value => 200},
-        :b_and_o_railroad => {:mortgage_value => 400},
-        :shortline_railroad => {:mortgage_value => 800}
+      :names=> {
+        :reading_railroad=>{:mortgage_value => 100},
+        :pennsylvania_railroad=>{:mortgage_value => 200},
+        :b_and_o_railroad=> {:mortgage_value => 400},
+        :shortline_railroad=> {:mortgage_value => 800}
       }
     }
   }
